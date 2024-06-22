aaf0428730d9dc6db2147a513b52d87e486f8b62
Deadline-Anderlias <151853690+Deadline-Anderlias@users.noreply.github.com>
2024-06-22 07:56:18 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route pour servir le fichier HTML principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Fonction pour gérer la soumission du formulaire
